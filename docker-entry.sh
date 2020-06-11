51d436a43514a013766a6ebd12a0953bd75e67f0
xavier <xavier@codenectar.com>
2020-06-11 09:55:15 +0200

    sleep option for kerbi

--- docker-entry.sh
+++ docker-entry.sh
@@ -12,6 +12,9 @@ elif [[ "$1" == "publish" ]]; then
   chmod 0600 /root/.gem/credentials
   gem build kerbi.gemspec
   gem push $(ls | grep ".gem$"); exit 0
+elif [[ "$1" == "sleep" ]]; then
+  echo "Going to sleep..."
+  while true; do sleep 30; done;
 else
   echo Bad args "$@"
 fi
