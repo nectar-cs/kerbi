cb630be4daf6a423c1ebca25796400de681ee515
xavier <xavier@codenectar.com>
2021-02-14 15:16:39 +0000

    utils.rb flatten_hash

--- lib/utils/utils.rb
+++ lib/utils/utils.rb
@@ -1,13 +1,27 @@
 module Kerbi
   module Utils
     class Utils
+
       class << self
         def real_files_for(*candidates)
           candidates.select do |fname|
             File.exists?(fname)
           end
         end
+
+        def flatten_hash(hash)
+          hash.each_with_object({}) do |(k, v), h|
+            if v.is_a? Hash
+              flatten_hash(v).map do |h_k, h_v|
+                h["#{k}.#{h_k}".to_sym] = h_v
+              end
+            else
+              h[k] = v
+            end
+          end
+        end
       end
+
     end
   end
 end
